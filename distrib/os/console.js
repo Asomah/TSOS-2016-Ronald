213a96a03a4717f56492c9662777f1d7545980e0
Asomah <asomahdart@yahoo.com>
2016-09-18 22:34:15 -0400

    scrolling, up and down arrows

--- distrib/os/console.js
+++ distrib/os/console.js
@@ -10,14 +10,13 @@
 var TSOS;
 (function (TSOS) {
     var Console = (function () {
-        function Console(currentFont, currentFontSize, currentXPosition, indexCmd, currentYPosition, buffer, prevStr, history, arrayOfCommands) {
+        function Console(currentFont, currentFontSize, currentXPosition, indexCmd, currentYPosition, buffer, history, arrayOfCommands) {
             if (currentFont === void 0) { currentFont = _DefaultFontFamily; }
             if (currentFontSize === void 0) { currentFontSize = _DefaultFontSize; }
             if (currentXPosition === void 0) { currentXPosition = 0; }
             if (indexCmd === void 0) { indexCmd = -1; }
             if (currentYPosition === void 0) { currentYPosition = _DefaultFontSize; }
             if (buffer === void 0) { buffer = ""; }
-            if (prevStr === void 0) { prevStr = ""; }
             if (history === void 0) { history = _ArrayOfHistory; }
             if (arrayOfCommands === void 0) { arrayOfCommands = _ArrayOfCommands = ["ver", "help", "shutdown", "cls", "man", "trace", "rot13",
                 "prompt", "date", "whereami", "time"]; }
@@ -27,7 +26,6 @@ var TSOS;
             this.indexCmd = indexCmd;
             this.currentYPosition = currentYPosition;
             this.buffer = buffer;
-            this.prevStr = prevStr;
             this.history = history;
             this.arrayOfCommands = arrayOfCommands;
         }
@@ -39,7 +37,7 @@ var TSOS;
             _DrawingContext.clearRect(0, 0, _Canvas.width, _Canvas.height);
         };
         Console.prototype.clearLine = function () {
-            _DrawingContext.clearRect(0, this.currentYPosition - this.currentFontSize, _Canvas.width, this.currentFontSize + 5);
+            _DrawingContext.clearRect(0, this.currentYPosition - this.currentFontSize - 2, _Canvas.width, this.currentFontSize + 7);
             this.currentXPosition = 0;
         };
         Console.prototype.resetXY = function () {
@@ -52,6 +50,12 @@ var TSOS;
                 var chr = _KernelInputQueue.dequeue();
                 // Check to see if it's "special" (enter or ctrl-c) or "normal" (anything else that the keyboard device driver gave us).
                 if (chr === String.fromCharCode(13)) {
+                    // Get current date and time
+                    var date = new Date().toDateString();
+                    var time = new Date().toLocaleTimeString();
+                    //Display dateTime and status when ever user enters command
+                    document.getElementById('Time').innerHTML = 'Date: ' + date + ' Time:' + time;
+                    document.getElementById('Status').innerHTML = 'Status: ' + this.buffer;
                     // The enter key marks the end of a console command, so ...
                     // ... tell the shell ...
                     _OsShell.handleInput(this.buffer);
@@ -61,7 +65,6 @@ var TSOS;
                     this.indexCmd = this.indexCmd + 1;
                     // ... and reset our buffer.
                     this.buffer = "";
-                    this.prevStr = "";
                 }
                 else if (chr === String.fromCharCode(8)) {
                     this.deleteText();
@@ -115,19 +118,29 @@ var TSOS;
             this.putText(">" + this.buffer);
         };
         Console.prototype.tab = function () {
+            /*
+            Sort array of valid commands
+            Compare buffer to the start string of the valid commands
+            if there is a match, stop the loop and set buffer to the specific valid command
+            draw buffer on canvas
+            */
             var newArray = _ArrayOfCommands.sort();
             var text = this.buffer;
             for (var i = 0; i < newArray.length; i++) {
-                if (newArray[i].indexOf(text) == 0 && this.prevStr != newArray[i]) {
+                if (newArray[i].indexOf(text) == 0) {
                     this.buffer = newArray[i];
-                    this.prevStr = newArray[i];
+                    break;
                 }
             }
             this.clearLine();
             this.putText(">" + this.buffer);
         };
         Console.prototype.upArrow = function () {
-            //var length = this.history.length;
+            /*Check to see if index of command history is not less than 0
+              Set buffer to command by using the index of that command in the commandHistory array
+              Clear line and put buffer back
+              Decrease index of command history by one if it is greater than 0
+            */
             if (this.indexCmd >= 0) {
                 this.buffer = this.history[this.indexCmd];
                 this.clearLine();
@@ -138,6 +151,11 @@ var TSOS;
             }
         };
         Console.prototype.downArrow = function () {
+            /*Check to see if index of command history is in the right range
+               Set buffer to command by using the index of that command in the commandHistory array
+               Clear line and put buffer back
+               Increase index of command history by one if it is less than the lenght of array -1.
+             */
             if (this.indexCmd < this.history.length && this.history.length != 0) {
                 this.buffer = this.history[this.indexCmd];
                 this.clearLine();
@@ -148,6 +166,8 @@ var TSOS;
             }
         };
         Console.prototype.scroll = function () {
+            /*Make a new canvas of the current y position is greater than the canvas height
+            */
             if (this.currentYPosition > _Canvas.height) {
                 var newCanvas = _DrawingContext.getImageData(0, _DefaultFontSize + 8, _Canvas.width, _Canvas.height);
                 this.clearScreen();
